0ae9a7416cf76a989b96394240118729e3d019f2
JulienDaCosta-GitHub <55091161+JulienDaCosta-GitHub@users.noreply.github.com>
2020-09-29 11:41:09 +0200

    New view

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs-locals');
+const path = require('path');
 
 app.get('/', (req, res) => {
-    res.send('Bonjour')
+    res.render('index')
+})
+app.get('/post', function(req, res) {
+    res.render('show')
 })
 
 app.use(express.static('public'));
+
+app.engine('ejs', ejs);
+app.set('view engine' , 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(3000, function() {
     console.log('Server démarré sur http://localhost:3000')
