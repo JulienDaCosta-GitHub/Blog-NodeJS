2cf828f3a518ff46b592ce1c834f0f5b29114f14
JulienDaCosta-GitHub <55091161+JulienDaCosta-GitHub@users.noreply.github.com>
2020-09-30 08:00:43 +0200

    fix

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -3,8 +3,8 @@ const path = require("path");
 const routes = require('../routes/index');
 
 module.exports = function (app) {
-    app.use(express.static('../../public'));
-    app.set('views', path.join(__dirname, '../views'));
+    app.use('/', express.static(path.resolve(__dirname, '../../public')));
+    app.set('views', path.resolve(__dirname, '../views'));
 
     app.use(routes());
 }
